9d84a4f5a4b1fe065993980650929fef62631fd5
Patrick Gunsolley <patrick.gunsolley@outlook.com>
2026-05-12 06:26:21 -0700

    Added crud routes for contact manager

--- plugins/ContactManager/config/routes.php
+++ plugins/ContactManager/config/routes.php
@@ -41,6 +41,105 @@ return function (RouteBuilder $routes): void {
                         );
                 }
             );
+            $builder->scope(
+                '/contact-emails', 
+                ['controller' => 'ContactEmails', '_namePrefix' => 'contactEmails:'],
+                function (RouteBuilder $builder) {
+                    $builder
+                        ->connect(
+                            '/',
+                            ['action' => 'index'],
+                            ['_name' => 'index'],
+                        );
+
+                    $builder
+                        ->connect(
+                            '/add',
+                            ['action' => 'add'],
+                            ['_name' => 'add'],
+                        );
+
+                    $builder
+                        ->connect(
+                            '/view',
+                            ['action' => 'view'],
+                            ['_name' => 'view'],
+                        );
+
+                    $builder
+                        ->connect(
+                            '/edit',
+                            ['action' => 'edit'],
+                            ['_name' => 'edit'],
+                        );
+                }
+            );
+            $builder->scope(
+                '/contact-phones', 
+                ['controller' => 'ContactPhones', '_namePrefix' => 'contactPhones:'],
+                function (RouteBuilder $builder) {
+                    $builder
+                        ->connect(
+                            '/',
+                            ['action' => 'index'],
+                            ['_name' => 'index'],
+                        );
+
+                    $builder
+                        ->connect(
+                            '/add',
+                            ['action' => 'add'],
+                            ['_name' => 'add'],
+                        );
+
+                    $builder
+                        ->connect(
+                            '/view',
+                            ['action' => 'view'],
+                            ['_name' => 'view'],
+                        );
+
+                    $builder
+                        ->connect(
+                            '/edit',
+                            ['action' => 'edit'],
+                            ['_name' => 'edit'],
+                        );
+                }
+            );
+            $builder->scope(
+                '/contact-links', 
+                ['controller' => 'ContactLinks', '_namePrefix' => 'contactLinks:'],
+                function (RouteBuilder $builder) {
+                    $builder
+                        ->connect(
+                            '/',
+                            ['action' => 'index'],
+                            ['_name' => 'index'],
+                        );
+
+                    $builder
+                        ->connect(
+                            '/add',
+                            ['action' => 'add'],
+                            ['_name' => 'add'],
+                        );
+
+                    $builder
+                        ->connect(
+                            '/view',
+                            ['action' => 'view'],
+                            ['_name' => 'view'],
+                        );
+
+                    $builder
+                        ->connect(
+                            '/edit',
+                            ['action' => 'edit'],
+                            ['_name' => 'edit'],
+                        );
+                }
+            );
         }
     );
 };
